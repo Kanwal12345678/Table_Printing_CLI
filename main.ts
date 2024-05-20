c103b576b33199b9a385436e2acbfdef9a493ae8
Kanwal Shahzadi <160924893+Kanwal12345678@users.noreply.github.com>
2024-05-20 20:55:48 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,29 @@
+//importing inquirer
+import inquirer from "inquirer"; 
+//importing chalk
+import chalk from "chalk";
+
+//printing the welcome message
+console.log(chalk.magenta.bold.italic(" \t\n || Wellcome to my Table Printing app || \t\n"));
+
+//declaring a boolean variable
+let myTable = true;
+
+//do while loop to print the table  
+while(myTable){
+    let num = await inquirer.prompt([{
+        name:"num",
+        type:"number",
+        message: chalk.yellowBright("Enter a number")
+    }])
+    let num1 = num.num;
+    for(let i = 1; i<=10; i++){
+        console.log(chalk.greenBright(`${num1} * ${i} = ${num1*i}`));
+    }
+    let answer = await inquirer.prompt([{
+        name:"again",
+        type:"confirm",
+        message: chalk.yellowBright("Do you want to continue?")
+    }])
+    myTable = answer.again;
+}
